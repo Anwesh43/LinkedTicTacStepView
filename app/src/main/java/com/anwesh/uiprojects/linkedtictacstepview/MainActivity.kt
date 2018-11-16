46c8c3a2e3241eb941f2d7d2fede28c264aea2b2
Anwesh43 <anweshthecool0@gmail.com>
2018-11-16 11:35:13 +0530

    adding TicTacStepView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtictacstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtictacstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtictacstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.tictacstepview.TicTacStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TicTacStepView.create(this)
     }
 }
