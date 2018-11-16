a7f44d4bc98445e7b0b33c1b4688cf79d3b54653
Anwesh43 <anweshthecool0@gmail.com>
2018-11-16 10:54:55 +0530

    adding TicTacStepView class

--- tictacstepview/src/main/java/com/anwesh/uiprojects/tictacstepview/TicTacStepView.kt
+++ tictacstepview/src/main/java/com/anwesh/uiprojects/tictacstepview/TicTacStepView.kt
@@ -91,3 +91,21 @@ fun Canvas.drawTTSNode(i : Int, scale : Float, paint : Paint) {
     drawTicTac(size, sc2, paint)
     restore()
 }
+
+class TicTacStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
