c2fda9d150df0783bc4f30c78acaf99cf0da37f5
Anwesh43 <anweshthecool0@gmail.com>
2018-11-16 23:40:35 +0530

    adding AnimationListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtictacstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtictacstepview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkedtictacstepview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.tictacstepview.TicTacStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TicTacStepView.create(this)
+        val view : TicTacStepView = TicTacStepView.create(this)
         fullScreen()
+        view.addOnAnimationListener({createToast("animation number $it is complete")}, {createToast("animation number $it is reset")})
+    }
+
+    private fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
