5d0ae0271c2a56ae98e4ca219550c2d1ad21314f
Anwesh43 <anweshthecool0@gmail.com>
2018-11-16 10:53:25 +0530

    adding drawTTSNode canvas extension method

--- tictacstepview/src/main/java/com/anwesh/uiprojects/tictacstepview/TicTacStepView.kt
+++ tictacstepview/src/main/java/com/anwesh/uiprojects/tictacstepview/TicTacStepView.kt
@@ -7,11 +7,8 @@ package com.anwesh.uiprojects.tictacstepview
 import android.view.View
 import android.view.MotionEvent
 import android.content.Context
-import android.graphics.Paint
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.RectF
 import android.app.Activity
+import android.graphics.*
 
 val nodes : Int = 5
 val lines : Int = 2
@@ -20,6 +17,7 @@ val scGap : Float = 0.1f / parts
 val scDiv : Double = 1.0 / parts
 val STROKE_FACTOR : Int = 60
 val SIZE_FACTOR : Int = 3
+val TIC_TAC_PARTS : Int = 3
 
 fun Int.getInverse() : Float = 1f / this
 
@@ -30,3 +28,66 @@ fun Float.getScaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.getMirrorValue(a : Int, b : Int) : Float = this * b.getInverse()  + (1 - this) * a.getInverse()
 
 fun Float.updateScale(a : Int, b : Int, dir : Float) : Float = dir * scGap * getScaleFactor().getMirrorValue(a, b)
+
+fun Int.getCenterPoint(size : Float) : PointF = PointF(size/2 + size * this % 3, size/2 + size * (this / 3))
+
+fun Int.getSquare() : Int = this * this
+
+fun Canvas.drawLines(size : Float, scale : Float, paint : Paint) {
+    val gap : Float = 2 * size / TIC_TAC_PARTS
+    val x : Float = -size + gap
+    val y : Float = -size
+    for (j in 0..(lines - 1)) {
+        val sc : Float = scale.divideScale(j, lines)
+        save()
+        rotate(90f * j)
+        for (k in 0..1) {
+            drawLine(x + size * k, -size, x + size * k, -size + 2 * size * sc, paint)
+        }
+        restore()
+    }
+}
+
+fun Canvas.drawTicTac(size : Float, scale : Float, paint : Paint) {
+    val gap : Float = 2 * size / (TIC_TAC_PARTS)
+    save()
+    translate(-size, -size)
+    for (j in 0..(TIC_TAC_PARTS.getSquare() - 1)) {
+        val x : Float = j.getCenterPoint(gap).x
+        val y : Float = j.getCenterPoint(gap).y
+        val sc : Float = scale.divideScale(j, TIC_TAC_PARTS.getSquare())
+        save()
+        translate(x, y)
+        if (j % 2 == 0) {
+            for (k in 0..1) {
+                val cSize : Float = (size / 4) * sc
+                save()
+                rotate(45f + 90f * k)
+                drawLine(0f, -cSize, 0f, cSize, paint)
+                restore()
+            }
+        } else {
+            drawArc(RectF(-size/4, -size/4, size/4, size/4),
+                    -90f, 360f * sc, false, paint)
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawTTSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / SIZE_FACTOR
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.strokeWidth = Math.min(w, h) / STROKE_FACTOR
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = Color.parseColor("#4A148C")
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawLines(size, sc1, paint)
+    drawTicTac(size, sc2, paint)
+    restore()
+}
