ac0f8af740fed6ed5fe48f1a3d1021a5315b9042
Anwesh43 <anweshthecool0@gmail.com>
2018-11-16 10:13:33 +0530

    adding Float and Int canvas extension methods

--- tictacstepview/src/main/java/com/anwesh/uiprojects/tictacstepview/TicTacStepView.kt
+++ tictacstepview/src/main/java/com/anwesh/uiprojects/tictacstepview/TicTacStepView.kt
@@ -20,3 +20,13 @@ val scGap : Float = 0.1f / parts
 val scDiv : Double = 1.0 / parts
 val STROKE_FACTOR : Int = 60
 val SIZE_FACTOR : Int = 3
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.getScaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.getMirrorValue(a : Int, b : Int) : Float = this * b.getInverse()  + (1 - this) * a.getInverse()
+
+fun Float.updateScale(a : Int, b : Int, dir : Float) : Float = dir * scGap * getScaleFactor().getMirrorValue(a, b)
